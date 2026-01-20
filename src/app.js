7aa42f455f2a253eda444517f210d29ee2def60c
jayalakshmi-ede <jayalakshmi.ede@copart.com>
2026-01-20 14:43:49 +0530

    Updated cors url

--- src/app.js
+++ src/app.js
@@ -4,33 +4,40 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    // origin: [
-    //   "http://localhost:5173",
-    //   "https://chicken-frontend-pi.vercel.app/"
-    // ],
-    origin: "https://chicken-frontend-pi.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (
+        origin.endsWith(".vercel.app") ||
+        origin === "http://localhost:5173"
+      ) {
+        return callback(null, true);
+      }
+
+      callback(new Error("CORS not allowed"));
+    },
     credentials: true,
-    // methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    // allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://chicken-frontend-pi.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://chicken-frontend-pi.vercel.app");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
 
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
-});
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.get("/", (req, res) => {
